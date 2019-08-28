6ac34b8f1d3f232c02c41fb11c495e7de72b4a15
29775 <rsha0004@student.monash.edu>
2019-08-28 10:13:11 +1000

    j

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get("/",function(req,res){
     let fileName=filepath+"inde.html";
     res.sendFile(fileName);
 });
+//nothing
 app.get("/addTask",function(req,res){
     let fileName=filepath+"index.html";
     res.sendFile(fileName);
